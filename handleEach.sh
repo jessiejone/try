5b1c23b7229d72ac1dd54d1d3afd58f8378d195d
jessiejone <jessiejones@126.com>
2018-09-18 15:46:06 +0800

    update the script file

---
+++ handleEach.sh
@@ -0,0 +1,16 @@
+case $1 in
+Monday)
+ echo PLease give me a rose;;
+Tuesday)
+ echo PLease give me a apple;;
+Wednesday)
+ echo Please give me a bottle of wine;;
+Thursday)
+ echo "I will read a poem for you";;
+Friday)
+ echo "I will sing a song for you";;
+Saturday)
+ echo "I will do cleaning for you";;
+Sunday)
+ echo "Please cook dinner for you";;
+esac
