fec075748c4d4f5e435205b39ac4fe221231e990
unknown <jessiejones@126.com>
2019-04-21 15:39:24 +0800

    update the scripts about loop

---
+++ ArrayHandle.sh
@@ -0,0 +1,22 @@
+#！/bin/bash
+days=(Monday Tuesday Wednesday Thursday Friday Saturday Sunday)
+for ((i=0; i<${#days[@]}; i++))
+do 
+ let "totalCount+=1" 
+done 
+
+echo ""
+
+for ((i=0; i < ${#days[@]}; i++))
+do
+ let "daysCount=$i+1"
+ echo "This is $daysCount of $totalCount" 
+ 
+ bash handleEach.sh ${days[i]}
+ let "errors=$?"
+ let "totalErrors+=errors"
+ echo error count is : $errors
+ echo ""
+done
+
+exit $totalErrors
