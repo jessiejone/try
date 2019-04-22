69715fadadc41dcbe7fa2a65b06bd580840c2f94
Xing <cnxinhui@elekta.com>
2019-04-22 09:56:11 +0800

    move the command to command.sh

--- CommonCommand.sh
+++ CommonCommand.sh
@@ -1,2 +1,8 @@
 #! /bin/bash
 grep -v '^$' Blanklinebefore > BlanklineAfter
+sed '/oldfunc/s/^/#: migrate to new func/' sample
+sed '/foo/,/bar/ s/o/a/' sample
+sed -n '1,3p' sample
+sed '/foo/! s/ar/arr/' sample
+cut -d : -f 1 /etc/passwd
+ls -l | cut -c 1-10
--- substitute.sh
+++
@@ -1,6 +0,0 @@
-sed '/oldfunc/s/^/#: migrate to new func/' sample
-sed '/foo/,/bar/ s/o/a/' sample
-sed -n '1,3p' sample
-sed '/foo/! s/ar/arr/' sample
-cut -d : -f 1 /etc/passwd
-ls -l | cut -c 1-10
