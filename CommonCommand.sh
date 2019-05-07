2215694f10e4dce3306ad65a87b16e43c9c8a6b4
Xing <cnxinhui@elekta.com>
2019-05-07 16:36:49 +0800

    add  new command

--- CommonCommand.sh
+++ CommonCommand.sh
@@ -12,5 +12,15 @@ awk -F : '{ print $1, $5 }' linuxpasswd
 awk -F : -v 'OFS=**' '{print $1, $5}' linuxpasswd
 #output the selected fields
 awk -F : '{ print "User", $1, "is really", $5}' linuxpasswd
-
-
+#BEGIN and END is the operation before scanning text
+awk 'BEGIN {print "*******"; FS=":"; OFS=" "} {print $1, $5} END {print "*******"}' linuxpasswd
+#remove the duplicated records
+cat test_sort.txt | sort -u
+#make the list reverse
+cat test_sort.txt | sort -r
+## 按照常规数值排序
+cat test_sort.txt |sort -g
+## 按照字符串数值排序
+cat test_sort.txt |sort -n
+#sort as the third field
+cat test_sort.txt | sort -t "|" -k 3
