493021ff25999ecaf625a2ad79f2413ef67858c0
unknown <jessiejones@126.com>
2019-05-26 19:30:55 +0800

    add some poerations

--- OperationsToParameters.sh
+++ OperationsToParameters.sh
@@ -1,4 +1,22 @@
 #add some combo to the output of parameters
 reminder="Time to go to the dentist"
-sleep 120
 echo _Time to go to the dentist_
+while [ $# != 0 ] #以shift逐渐减少$#,循环将会终止
+do 
+echo $1
+shift             #移开第一个参数
+done
+#$*将所有命令行参数视为一个单个字符串
+#$@将所有命令行参数视为一个个单独的字符串
+#set -- 命令可以设置位置参数的值，并将之前存在的任何值q丢弃
+set -- hello "hi there" greetings #自hellos开始设置新的位置参数
+echo there are 3 total arguments
+for i in $* #在没有双引号的情况下，$*和$@是一样的
+do echo i is $i
+done
+for i in "$@" #加了双引号，$@保留真正的参数值
+do echo i is $i
+done
+for i in "$*" #加了双引号，$*表示一个字符串
+do echo i is $i
+done
